dd116c56300a947c043bdb1673102a52980e49a5
Richard Wilson <richard@freerunningtech.com>
2014-05-05 15:08:25 -0700

    Rewrite User has many gift cards to exclude carted gcs
    When you add a gift card to the cart, and buy it for yourself or anyone
    else, before purchasing it it is actually visible on the gift cards page
    and anywhere else that grabs gift cards with user.gift_cards.
    
    This ensures that the association will exclude all gift cards on orders
    with a state of balance_due. If the gift card was created by an
    administrator it won't have a line_item_id so that stipulation exists as
    well

--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -1,6 +1,44 @@
 require 'spec_helper'
 
 describe Spree::User do
+  describe "has_many gift cards" do
+    let!(:user) { create :user }
+    let!(:gift_card) { create :gift_card, user: user }
+
+    subject { user.gift_cards.reload }
+
+    context "when gift cards were purchased in an order" do
+      let!(:product) { create :product, is_gift_card: true }
+      let!(:line_item) { create :line_item, product: product, order: order, gift_card: gift_card }
+
+      before do
+        gift_card.update_attributes(line_item: line_item)
+      end
+
+      context "has an outtanding balance" do
+        let!(:order) { create :order }
+
+        it "they are not included in the association query" do
+          expect(subject).to_not include(gift_card)
+        end
+      end
+
+      context "when the order doesn't have an outstanding balance" do
+        let!(:order) { create :order_ready_to_ship }
+
+        it "they are included in the association query" do
+          expect(subject).to include(gift_card)
+        end
+      end
+    end
+
+    context "when gift cards are created by administrators" do
+      it "is included in the association query" do
+        expect(subject).to include(gift_card)
+      end
+    end
+  end
+
   describe "#available_gift_cards" do
     let(:user) { create :user }
     let(:order) { create :order, user: user }
